30021f83e94ba39f2965790f8bb36dba83376d9c
grifon23 <shuklin.eduard.ua@gmail.com>
2022-12-02 09:58:40 +0200

    DRAFT | login screen

---
+++ lib/components/login_form.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+
+
+    class LoginForm extends StatefulWidget {
+      const LoginForm({super.key});
+
+      @override
+      LoginFormState createState() => LoginFormState();
+  }
+
+  class LoginFormState extends State<LoginForm>{
+  final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
+final controllerMail = TextEditingController();
+final controllerPassword = TextEditingController();
+  @override
+    Widget build(BuildContext context) {
+    return Form(
+      key: _globalKey,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+
+          children: [
+            TextFormField(
+              controller: controllerMail,
+            textInputAction: TextInputAction.next,
+            focusNode: FocusNode(),
+              validator: (val)  {
+                if(val == null || val.isEmpty){
+                  return 'Field is required';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email_outlined, size: 20,),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                    style: BorderStyle.solid
+                  )
+                ),
+
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 1.0,
+                    style: BorderStyle.solid
+                  )
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 1.0,
+                      style: BorderStyle.solid)
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 1.0,
+                    style: BorderStyle.solid
+                  )
+                ),
+                border: OutlineInputBorder(),
+                label: Text('Email'),
+
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+
+            //password
+            TextFormField(
+              controller: controllerPassword,
+              validator: (val)  {
+                if(val == null || val.isEmpty){
+                  return 'Field is required';
+                }
+                return null;
+              },
+
+              obscureText: true,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.lock_outline, size: 20,),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                        color: Colors.grey,
+                        style: BorderStyle.solid
+                    )
+                ),
+
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1.0,
+                        style: BorderStyle.solid
+                    )
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                        color: Colors.redAccent,
+                        width: 1.0,
+                        style: BorderStyle.solid)
+                ),
+                errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 1.0,
+                        style: BorderStyle.solid
+                    )
+                ),
+                border: OutlineInputBorder(),
+                label: Text('Password'),
+              ),
+
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            //sign in btn
+               ElevatedButton(
+
+                  onPressed: (){
+                    if(_globalKey.currentState!.validate()){
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
+                  child:Center(child: Text('Sign In', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),)),),
+
+            SizedBox(
+              height: 5,
+            ),],
+        ),
+      )
+    );
+  }
+
+  }
---
+++ lib/screens/login_page.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: always_specify_types
+
+import 'package:cooking_book/components/login_form.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+            child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+             Icon(
+                Icons.account_circle,
+                size: 60,
+              ),
+             SizedBox(
+                height: 25,
+              ),
+              //Hello again!
+             Text(
+                'Hello Again',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+             Text(
+                'Wellcome back, you\'ve been missed!',
+                style: TextStyle(fontSize: 20),
+              ),
+             SizedBox(
+                height: 30,
+              ),
+              LoginForm(),
+
+             Text('Register now', style: TextStyle(fontSize: 16, color: Colors.blue, fontWeight: FontWeight.w500),)
+            ],
+          ),
+        )));
+  }
+}
